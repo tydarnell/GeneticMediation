178dd12a4003c152535994710e88f464eca10baa
Ty Darnell <tydarnell@gmail.com>
2020-04-25 14:57:03 -0400

    change license

---
+++ R/GeneticMediation.R
@@ -0,0 +1,9 @@
+#' GeneticMediation: A package for conducting causal mediation analysis on ROSMAP data
+#'
+#' GeneticMediation provides methods for conducting causal mediation analysis on data from the ROSMAP study.
+#' It also provides methods for cleaning, matching, and preparing the data for analysis.
+#'
+#' @docType package
+#' @name GeneticMediation
+NULL
+#> NULL
