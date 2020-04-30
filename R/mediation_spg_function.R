335167fd468c50cc7d56e329f473569772658423
Ty Darnell <tydarnell@gmail.com>
2020-04-29 20:27:33 -0400

    fix mediation_spg

--- R/mediation_spg_function.R
+++ R/mediation_spg_function.R
@@ -1,14 +1,14 @@
-#' Mediation Summary SNP-Peak-Gene 
+#' Mediation SNP-Peak-Gene 
 #'
-#' @description Mediation summary for one SNP-Peak-Gene match
+#' @description Mediation analysis one SNP-Peak-Gene match
 #' @param med.res.data gene mediation data
 #' @param match_row row in match dataframe: col1 SNP, col2 Peak, col3 Gene
 #' @param simulations number of simulations, 1000 by default
 #' @keywords mediation summary
 #' @export
 
-med_summary_spg <- function(med.res.data, match_row, simulations=1000) {
-  names(match_row)=NULL
+mediation_spg <- function(med.res.data, match_row, simulations=1000) {
+  match_row=purrr::as_vector(match_row)
   covar = "age+membership+PC1+PC2+PC3"
   a = match_row[1]
   m = match_row[2]
@@ -18,5 +18,5 @@ med_summary_spg <- function(med.res.data, match_row, simulations=1000) {
   fm2 <- paste(gene, "~", m, "+", a, "+", covar, sep = " ")
   out.fit <- stats::lm(fm2, data = med.res.data)
   med.out <- mediation::mediate(med.fit, out.fit, treat = a, mediator = m, sims = simulations)
-  summary(med.out)
+  med.out
 }
